d2eb9d4221e80f509fc626741c19677b0d8fc94f
Arne Peirs <arnepeirs@hotmail.com>
2017-04-19 11:19:23 +0200

    Set version to 1.3

--- NetKAN/build.sh
+++ NetKAN/build.sh
@@ -6,7 +6,7 @@ set -e
 shopt -s nullglob
 
 # Default flags.
-KSP_VERSION_DEFAULT="1.2.9"
+KSP_VERSION_DEFAULT="1.3"
 KSP_NAME_DEFAULT="dummy"
 
 # Locations of CKAN and NetKAN.
@@ -79,11 +79,19 @@ create_dummy_ksp () {
         KSP_VERSION="1.1.3"
         ;;
     "1.2")
-        echo "Overriding '1.2' with '$KSP_VERSION_DEFAULT'"
-        KSP_VERSION=$KSP_VERSION_DEFAULT
+        echo "Overriding '1.2' with '1.2.2'"
+        KSP_VERSION="1.2.2"
         ;;
     "1.2.99")
-        echo "Overriding '1.2.99' with '$KSP_VERSION_DEFAULT'"
+        echo "Overriding '1.2.99' with '1.2.2'"
+        KSP_VERSION="1.2.2"
+        ;;
+    "1.3")
+        echo "Overriding '1.3' with '$KSP_VERSION_DEFAULT'"
+        KSP_VERSION=$KSP_VERSION_DEFAULT
+        ;;
+    "1.3.99")
+        echo "Overriding '1.3.99' with '$KSP_VERSION_DEFAULT'"
         KSP_VERSION=$KSP_VERSION_DEFAULT
         ;;
     "1.99.99")
