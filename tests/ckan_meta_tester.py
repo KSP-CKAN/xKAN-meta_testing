f3653529d3f5c3940f0402fe2541d585ab308e2b
Paul Hebble <pjhebble@gmail.com>
2020-12-18 17:04:30 -0600

    Don't tell the bot to trust the bot

--- tests/ckan_meta_tester.py
+++ tests/ckan_meta_tester.py
@@ -6,5 +6,5 @@ from ckan_meta_tester.ckan_meta_tester import CkanMetaTester
 class TestCkanMetaTester(unittest.TestCase):
 
     def test_true(self) -> None:
-        tester = CkanMetaTester()
+        tester = CkanMetaTester(False)
         self.assertTrue(tester.test_metadata())
